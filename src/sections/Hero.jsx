493c2539a7fbe685c41ac1a8a09c294bde9cc27d
yslanlopes12 <yslanl2005@gmail.com>
2026-04-01 12:15:45 -0300

    feat: add Navbar & hero, adjustment in responsiveness

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,13 @@
+import HeroText from "../components/HeroText"
+const Hero = () => {
+  return (
+    <section 
+    className="flex items-start justify-center 
+    md:items-start 
+    md:justify-start min-h-screen overflow-hidden c-space">
+      <HeroText />
+    </section>
+  )
+}
+
+export default Hero
