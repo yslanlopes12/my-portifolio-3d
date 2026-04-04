bad95140afc8b490ec9945f6d7d16cb1e2b26d97
yslanlopes12 <yslanl2005@gmail.com>
2026-04-03 21:38:43 -0300

    feat: astronaut 3d

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,24 +1,27 @@
 import HeroText from "../components/HeroText"
-import { Canvas } from "react-three-fiber";
+import { Canvas } from '@react-three/fiber';
 import ParallaxBackground from "../components/ParallaxBackground.jsx";
-import Astronaut from "../components/Astronaut.jsx";
+import {Astronaut} from "../components/Astronaut.jsx";
+import {OrbitControls} from "@react-three/drei";
 
 const Hero = () => {
   return (
-    <section 
-    className="flex items-start justify-center 
-    md:items-start 
+    <section
+    className="flex items-start justify-center
+    md:items-start
     md:justify-start min-h-screen overflow-hidden c-space">
       <HeroText />
       <ParallaxBackground />
         <figure className="absolute inset-0"
         style={{ width: "100vw", height: "100vh" }}
         >
-            <Canvas>
-                <Astronaut />
+            <Canvas camera={{ position: [0, 1, 3] }}>
+                <Astronaut scale={0.23} position={[0, -1.5, 0]} />
+                {/*<orbitControls />*/}
             </Canvas>
         </figure>
     </section>
-  )
-}
-export default Hero
+  );
+};
+export default Hero;
+//42:55
